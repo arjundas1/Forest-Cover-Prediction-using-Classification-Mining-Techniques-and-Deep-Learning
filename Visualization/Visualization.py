16a310a26eac20e4fa99c23e62e20b955ce6735f
John Harshith <83835729+JohnHarshith@users.noreply.github.com>
2021-12-08 16:29:11 +0530

    Update Visualization.py

--- Visualization/Visualization.py
+++ Visualization/Visualization.py
@@ -6,7 +6,7 @@ dataset = pd.read_csv('covtype.csv', index_col=0)
 
 # Class distribution
 a = dataset['Cover_Type']
-sns.countplot(a)
+sns.countplot(a,palette=["#AFEEEE","#B0C4DE"])
 
 # Correlation
 size = 10
@@ -24,37 +24,19 @@ s_corr_list = sorted(corr_list, key=lambda x: -abs(x[0]))
 # Scatter Plot (Pair plot)
 for v, i, j in s_corr_list:
   sns.pairplot(dataset, hue="Cover_Type", height=6, x_vars=cols[i], y_vars=cols[j])
+  sns.color_palette("flare", as_cmap=True)
   plt.show()
 
 # Heat Map
 col_list = dataset.columns
 col_list = [col for col in col_list if not col[0:4] == 'Soil']
 fig, ax = plt.subplots(figsize=(10, 10))
-sns.heatmap(dataset[col_list].corr(), square=True, linewidths=1)
+sns.heatmap(dataset[col_list].corr(), square=True, linewidths=1, cmap="Blues")
 plt.title('Correlation of Variables')
 plt.show()
 
-# Box Plot
-plt.figure(figsize=(10, 10))
-sns.boxplot(y='Slope', x='Cover_Type', data=dataset)
-plt.title('slope vs Cover_Type')
-plt.show()
-
-# Pair Plot
-sns.pairplot(dataset, hue='Cover_Type', vars=['Aspect', 'Slope', 'Hillshade_9am', 'Horizontal_Distance_To_Roadways', 'Horizontal_Distance_To_Hydrology', 'Horizontal_Distance_To_Fire_Points'], diag_kind="kde")
-plt.show()
-
-# Violin Plot
-cols = dataset.columns
-size = len(cols)-1
-x = cols[size]
-y = cols[0:size]
-for i in range(0, size):
-	sns.violinplot(data=dataset, x=x, y=y[i])
-	plt.show()
-
 # Horizontal_Distance_To_Hydrology & Vertical_Distance_To_Hydrology with Soil_Type1
-sns.lmplot(x='Horizontal_Distance_To_Hydrology', y='Vertical_Distance_To_Hydrology', data=dataset, hue='Soil_Type1', fit_reg=False)
+sns.lmplot(x='Horizontal_Distance_To_Hydrology', y='Vertical_Distance_To_Hydrology', data=dataset, hue='Soil_Type1', fit_reg=False, palette="Blues_r")
 
 # Horizontal_Distance_To_Hydrology & Vertical_Distance_To_Hydrology with Wilderness_Area2
-sns.lmplot(x='Horizontal_Distance_To_Hydrology', y='Vertical_Distance_To_Hydrology', data=dataset, hue='Wilderness_Area2',fit_reg=False)
+sns.lmplot(x='Horizontal_Distance_To_Hydrology', y='Vertical_Distance_To_Hydrology', data=dataset, hue='Wilderness_Area2',fit_reg=False, palette="Blues_r")
